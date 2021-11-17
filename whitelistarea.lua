e589d10b65bb2bbddc97d932ae588cd840a9a93e
Christhegod2662 <92185697+Christhegod2662@users.noreply.github.com>
2021-11-16 18:02:15 -0600

    Update whitelistarea.lua

--- whitelistarea.lua
+++ whitelistarea.lua
@@ -1,8 +1,17 @@
 local joe = {
-    1,
+    1997456916,
+    1957770900,
+    2314442300,
+    2857471228,
+    1237822618,
+    945431303,
+    1957768748,
+    2977926684,
+    942613091,
+    2261064963,
 }
 
-local realtacoppl = {}
+local realtacoppl = {2526537975,2025867999,171832854,171832169,}
 
 local function main()
     for i,v in pairs(game.Players:GetChildren()) do
