0fbb5be091549d9bb85f05ef57d4095002b160c6
Juice7mod <92185697+Christhegod2662@users.noreply.github.com>
2022-06-01 20:31:58 -0500

    Update whitelistarea.lua

--- whitelistarea.lua
+++ whitelistarea.lua
@@ -38,16 +38,16 @@ local function main()
     for i,v in pairs(game.Players:GetChildren()) do
         if table.find(realtacoppl,v.UserId) then
             if v.Character then
-                v.Character:FindFirstChild("Humanoid").DisplayName = '[💎]'..v.DisplayName
+                v.Character:FindFirstChild("Humanoid").DisplayName = '[👑]'..v.DisplayName
             end
         elseif table.find(joe,v.UserId) then
             if v.Character then
-                v.Character:FindFirstChild("Humanoid").DisplayName = '[⭐]'..v.DisplayName
+                v.Character:FindFirstChild("Humanoid").DisplayName = '[🍗]'..v.DisplayName
             end
         else
             if v.Character then
                 if not v.Character.LeftFoot:FindFirstChild("OriginalSize") then
-                    v.Character:FindFirstChild("Humanoid").DisplayName = '[🌮]'..v.DisplayName
+                    v.Character:FindFirstChild("Humanoid").DisplayName = '[🍖]'..v.DisplayName
                 end
             end
         end
